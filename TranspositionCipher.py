0a798b4558a54b005df83bff6cd7c17b01039906
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2024-08-25 17:47:36 -0400

    add get_columns; key can be given as a string

--- TranspositionCipher.py
+++ TranspositionCipher.py
@@ -1,7 +1,21 @@
 class TranspositionCipher(object): 
-        
+    
     def __init__(self, key):
-        self.key = key
+        if isinstance(key, str):
+            self.key = key
+            self.key_length = len(key)
+        if isinstance(key, int):
+            self.key = key
+     
+    @staticmethod       
+    def get_columns(self) -> list:
+        unique_chars = []
+        KEY_UPPER = self.key.upper()
+        for i, c in enumerate(KEY_UPPER):
+            unique_chars.append(c + str(i))
+        return unique_chars
+        
+        
 
 if __name__ == "__main__":
     pass
